757a9653cfaa824b60e4ab2c10b37aed8b6fc720
John Wong <johnwongapi@gmail.com>
2025-06-09 12:00:04 +0800

    Update run.sh

--- demo/airflow/run.sh
+++ demo/airflow/run.sh
@@ -2,4 +2,4 @@
 set -euo pipefail
 
 helm repo add apache-airflow https://airflow.apache.org
-microk8s helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace -f value.yaml
+helm upgrade --install airflow apache-airflow/airflow --namespace airflow --create-namespace -f value.yaml
