546bd350fa6eb185c9140486bf8bef149ad8f77c
Felix Chung <felix.chung@felixchung.org>
2025-06-14 21:48:06 +0800

    Change to use Dremio instead of Denodo

--- demo/dataset-propagate/files/dataset-propagate.py
+++ demo/dataset-propagate/files/dataset-propagate.py
@@ -13,6 +13,57 @@ sandbox_password = os.environ['SANDBOX_PASSWORD']
 prod_username = os.environ['PROD_USERNAME']
 prod_password = os.environ['PROD_PASSWORD']
 
+def login(username, password, dremioServer):
+  # we login using the old api for now
+  loginData = {'userName': username, 'password': password}
+  headers = {'content-type':'application/json'}
+  response = requests.post('{server}/apiv2/login'.format(server=dremioServer), headers=headers, data=json.dumps(loginData))
+  data = json.loads(response.text)
+  # retrieve the login token
+  token = data['token']
+  return token
+
+accesstoken_sandbox = login(sandbox_username, sandbox_password, sandbox_url)
+accesstoken_production = login(prod_username,prod_password,prod_url)
+
+def query(payload,env):
+    if env == 'PROD':
+        endpoint = prod_url
+        token = accesstoken_production
+    else:
+        endpoint = sandbox_url
+        token = accesstoken_sandbox
+        
+    response = requests.post(
+        f"{endpoint}/api/v3/sql",
+        headers={"Authorization": f"Bearer {token}"},
+        json=payload
+    )   
+        
+    if response.status_code == 200:
+        jobId = response.json().get("id")
+        while True:
+            response = requests.get(
+                f"{endpoint}/api/v3/job/{jobId}",
+                headers={"Authorization": f"Bearer {token}"},
+            )
+            if response.status_code == 200:
+                jobStatus = response.json().get("jobState")
+                if jobStatus == "COMPLETED":
+                    result = requests.get(
+                                    f"{endpoint}/api/v3/job/{jobId}/results",
+                                    headers={"Authorization": f"Bearer {token}"},
+                                )
+                    rows = result.json().get("rows")
+                    return rows
+                if jobStatus == "CANCELED" or jobStatus == "FAILED":
+                    return None
+            else:  
+                break
+    else:
+        print(f"Failed to get job details")
+        return FileNotFoundError
+        
 @app.after_request
 def after_request(response):
     # Only add CORS headers if the Origin header exists and is from localhost
@@ -28,8 +79,9 @@ def after_request(response):
 def PropagateDatasets():
     logging.info('PropagateDatasets function processed a request.')
     env = request.args.get('env')
-    db = request.args.get('db')
+    # db = request.args.get('db')
     view = request.args.get('view')
+    schema = request.args.get('schema')
     count = request.args.get('count', type = str)
 
     if env == 'PROD':
@@ -41,40 +93,26 @@ def PropagateDatasets():
         username = sandbox_username
         password = sandbox_password
 
-    if count is None:
-        params = {
-            '$format': 'json'
-        }
-    elif count.isdigit():
-        params = {
-            '$format': 'json'
-            ,'$count': count
-        }
+    sql_str = f"""
+                select 
+                    *
+                from 
+                    "{schema}"."{view}"
+                """
+    if count is not None and count.isdigit():
+        sql_str = sql_str + f" limit {count}"
     else:
         return jsonify({"message": "Parameter count is missing or not numeric."}), 401
-
-    if db:
-        response = requests.get(
-            f"{url}/{db}/views/{view}",
-            auth=HTTPBasicAuth(username, password),
-            params=params
-        )
     
-    else:
-        return jsonify({"message": "No db parameter provided"}), 400
+    payload = {
+        "sql": sql_str
+    } 
+    
+    response = query(payload,None)
 
-
-    if response.status_code == 200:
-        response_json = response.json()
-        data_rows = response_json['elements']
-        for data in data_rows:
-            data.pop('links', None)
-        json_data = json.dumps(data_rows)
-    else:
-        print(f"Failed to get metadata for dataset: {response.content}")
-
-    # return json.dumps(data_rows)
-    return Response(json_data, mimetype='application/json')
+    if response is not None:
+        response_json = json.dumps(response)
+        return Response(response_json, mimetype='application/json')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=6005)
